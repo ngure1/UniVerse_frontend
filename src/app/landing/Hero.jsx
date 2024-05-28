20014952125eaa3e68d2431b4f097e04b525a75f
kingoriwangechi <florencekingori00@gmail.com>
2024-05-28 12:56:58 +0300

    header,hero,about added

--- src/app/landing/Hero.jsx
+++ src/app/landing/Hero.jsx
@@ -1,32 +1,38 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+// import VideoBg from "@/components/VideoBg";
 import { Element } from "react-scroll";
 
 const Hero = () => {
 	return (
 		<Element
 			name="Home"
-			className="inline-flex items-center gap-[6.875rem] lg:mx-[9.75rem] md:mx-[6.75rem] sm:mx-0 h-screen "
+			className="flex flex-col justify-center items-center w-[90rem] h-[44.25rem] py-[3.25rem] px-0 gap-[3.25rem] shrink-0"
 		>
-			<div className="flex flex-col justify-center items-start gap-[1.875rem]">
-				<h1 className="heading-1 w-[40.625rem]">
-					Welcome to Universe: Empowering JKUAT Computing Alumni
+		
+			<div className="flex flex-col justify-center items-center w-[56.25rem] padding-[0.625rem] gap-[0.5rem] ">
+				<h1 className="heading-1 w-[50.625rem] text-center self-stretch text-[#00BD9D] font-nico-moji">
+					UniVerse
 				</h1>
-				<p className="body-text w-[40.625rem] h-[15.75rem] flex-shrink-0">
-					Join a vibrant community of JKUAT Computing alumni on Universe, your
-					centralized platform for seamless communication, collaboration, and
-					resource-sharing. Connect with fellow alumni, share insights, explore
-					career opportunities, and stay updated on departmental events.
-					Together, let's foster a thriving ecosystem of learning and growth
-					within our community.
+
+			<div className="w-[50rem] text-center">
+			<p className="sub-heading-2">
+					Welcome to JKUAT alumni connect.
 				</p>
-				<div className="flex gap-[3.125rem]">
-					<Button variant="outline">Login</Button>
-					<Button>signup</Button>
-				</div>
+				<p className="sub-heading-2">
+					Your hub for networking collaboration and career growth.
+				</p>
 			</div>
-			<div className="w-[35rem] h-[32.875rem] bg-slate-700"></div>
+				</div>
+				<div className="flex gap-[3.125rem]">
+
+				
+				<Button variant="outline" className="flex py-[0.625rem] px-[0.75rem] justify-center items-center gap-[0.5rem] rounded-[0.625rem] border border-solid border-[#F3FBF9] ">
+                <img  src="./images/googleLogo.png" alt="Google Logo" className="w-[1.875rem] h-[1.875rem]" />
+                <span className="text-black body-text">Continue with Google</span></Button>
+        
+				</div>
 		</Element>
 	);
 };
