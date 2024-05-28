c7a30a246e05ad952e063779edd9561254a765a6
ngure1 <ngugijoseph700@gmail.com>
2024-05-28 15:47:29 +0300

    fix: fix landing page error

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
-import { Team,Partners,LatestPosts } from "./landing/index";
+import {
+	Header,
+  Hero,
+	About,
+	Community,
+	LatestPosts,
+  Team,
+	Partners,
+} from "./landing/index";
 import Footer from "@/components/ui/MyComponents/Footer";
 export default function Home() {
 	return (
   <main className="flex flex-col gap-[8.75rem] h-full">
-    {/* <div className="flex items-center mt-[3rem]">
-      
-      <Button size="lg" className="mx-5" >Click</Button>
-      <ModeToggle/>
-      <p className="text-primary">Test colors</p>
-    </div> */}
+    <Header/>
+    <Hero/>
+    <About/>
+    <Community/>
     <LatestPosts/>
     <Team/>
     <Partners/>
