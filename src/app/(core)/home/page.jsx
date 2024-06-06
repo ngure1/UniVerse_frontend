3ae575ac6379f421d4d5ce6409d02caaa99de830
kingoriwangechi <florencekingori00@gmail.com>
2024-06-06 22:07:53 +0300

    feat: Add new pages for HomePage, Jobs, News, Events, and Support

--- src/app/(core)/home/page.jsx
+++ src/app/(core)/home/page.jsx
@@ -1,7 +1,31 @@
+"use client";
 import React from "react";
+import PostImage from "@/../public/images/postImage.png";
+import PostCard from "@/components/ui/MyComponents/PostCard";
+import ProfilePic from "@/../public/images/ProfilePic.jpeg";
 
 const Home = () => {
-	return <div>Home Page</div>;
+	return (
+		<div className="flex flex-col gap-4">
+			<PostCard
+				name="Jane Doe"
+				pfpImage={"https://github.com/shadcn.png"}
+				isVerified
+				type="Student"
+				date="1 Month Ago"
+				content="Exploring the intersection of technology, design, and innovation, our blog offers insights, tips, and  .....Read More"
+				postImage={PostImage}
+			/>
+			<PostCard
+				name="Jane Doe"
+				type="Student"
+				pfpImage={"../../../../public/images/ProfilePic.jpeg"}
+				date="1 Month Ago"
+				content="Exploring the intersection of technology, design, and innovation, our blog offers insights, tips, and  .....Read More"
+				postImage={PostImage}
+			/>
+		</div>
+	);
 };
 
 export default Home;
