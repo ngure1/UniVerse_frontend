d4fd62fcacd2b1d1bb5d181ff735b2703c099281
mwangi-hilda <mwangihw06@gmail.com>
2024-06-07 08:37:46 +0300

    Add Skeleton

---
+++ src/components/ui/MyComponents/Skeleton.jsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "../shadcnComponents/skeleton";
+
+import React from "react";
+
+const PostSkeleton = () => {
+	return <Skeleton className="w-[100px] h-[20px] rounded-full" />;
+};
+
+export default PostSkeleton;
