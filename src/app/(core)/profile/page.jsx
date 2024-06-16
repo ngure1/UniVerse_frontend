62ce8e45a99f2c1fc742a82e80061ed9e7d31595
ngure1 <ngugijoseph700@gmail.com>
2024-06-16 13:37:19 +0300

    refactor :recycle: : Update profile page to use profileData instead of data

--- src/app/(core)/profile/page.jsx
+++ src/app/(core)/profile/page.jsx
@@ -3,12 +3,11 @@ import { ModeToggle } from "@/components/ui/MyComponents/ModeToggle";
 import ProfileCard from "@/components/ui/MyComponents/cards/ProfileCard";
 import ProfileTabs from "@/components/ui/MyComponents/profile/ProfileTabs";
 import { useProfileMeQuery } from "@/redux/features/profiles/profileApiSlice";
-import { userAgent } from "next/server";
 import { toast } from "react-toastify";
 
 const ProfilePage = () => {
-	const { data, isLoading, error } = useProfileMeQuery(null);
-	console.log(data);
+	const { data: profileData, isLoading, error } = useProfileMeQuery(null);
+	console.log(profileData);
 
 	if (isLoading) {
 		return (
@@ -25,22 +24,22 @@ const ProfilePage = () => {
 	return (
 		<div className="flex flex-col gap-4 w-[100%]">
 			<div className="px-[2.5rem] py-[1.5rem] w-[80%] bg-white dark:bg-muted rounded-md flex flex-col gap-8">
-				{data && (
+				{profileData && (
 					<ProfileCard
-						first_name={data.user.first_name}
-						last_name={data.user.last_name}
-						profile_picture={data.profile_picture}
-						email={data.user.email}
-						address={data.address}
-						is_verified={data.is_verified}
-						is_alumni={data.is_alumni}
-						is_student={data.is_student}
-						is_lecturer={data.is_lecturer}
-						bio={data.bio}
-						phone_number={data.phone_number}
-						follower_count={data.followers_count}
-						following_count={data.following_count}
-						linked_in_url={data.linked_in_url}
+						first_name={profileData.user.first_name}
+						last_name={profileData.user.last_name}
+						profile_picture={profileData.profile_picture}
+						email={profileData.user.email}
+						address={profileData.address}
+						is_verified={profileData.is_verified}
+						is_alumni={profileData.is_alumni}
+						is_student={profileData.is_student}
+						is_lecturer={profileData.is_lecturer}
+						bio={profileData.bio}
+						phone_number={profileData.phone_number}
+						follower_count={profileData.followers_count}
+						following_count={profileData.following_count}
+						linked_in_url={profileData.linked_in_url}
 					/>
 				)}
 			</div>
