1cb1d2dac46bcfda71b017147a602872e4f15740
ngure1 <ngugijoseph700@gmail.com>
2024-06-22 17:12:11 +0300

    feat :sparkles: :  add image preview

--- src/components/forms/postForm.jsx
+++ src/components/forms/postForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import postFormSchema from "@/schema/postFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -17,6 +17,7 @@ import { usePostCreateMutation } from "@/redux/features/posts/postsApiSlice";
 import { toast } from "react-toastify";
 import Tiptap from "../ui/MyComponents/RichTextEditor/TipTap";
 import { useDialog } from "@/hooks/responsiveDialog";
+import Image from "next/image";
 
 const PostForm = () => {
 	// handling form default values and media upload
@@ -63,88 +64,106 @@ const PostForm = () => {
 	};
 
 	const { handleCloseDialog: closePostDialog } = useDialog("post");
-
+	const [previewUrl, setPreviewUrl] = useState(null);
 	return (
-		<div className="">
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					action=""
-					encType="multipart/form-data"
-					className="space-y-[1.5rem]">
-					<FormField
-						control={form.control}
-						name="title"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Title</FormLabel>
-								<FormControl>
-									<Input
-										placeholder="Your Post Title Goes Here"
-										type="text"
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="content"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Content</FormLabel>
-								<FormControl>
-									{/* <Input
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				action=""
+				encType="multipart/form-data"
+				className="space-y-[1.5rem]">
+				<FormField
+					control={form.control}
+					name="title"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Title</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Your Post Title Goes Here"
+									type="text"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="content"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Content</FormLabel>
+							<FormControl>
+								{/* <Input
 										placeholder="Type here..."
 										type="text"
 										{...field}
 									/> */}
-									<Tiptap
-										onChange={field.onChange}
-										content={field.name}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+								<Tiptap
+									onChange={field.onChange}
+									content={field.name}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{previewUrl && (
+					<div className="rounded-[0.25rem] min-h-[30rem] w-[80%] relative">
+						<Image
+							src={previewUrl}
+							alt="Preview Image"
+							layout="fill"
+							objectFit="cover"
+						/>
+						{/* <video
+							src={previewUrl}
+							width="320"
+							height="240"
+							controls></video> */}
+					</div>
+				)}
 
-					<FormField
-						control={form.control}
-						name="media"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Media Upload</FormLabel>
-								<FormControl>
-									<Input
-										type="file"
-										{...mediaRef}
-										onChange={(event) => {
-											field.onChange(
-												event.target?.files?.[0] ??
-													undefined,
-											);
-										}}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+				<FormField
+					control={form.control}
+					name="media"
+					render={({ field }) => (
+						<FormItem className>
+							<FormLabel>Media Upload</FormLabel>
+							<FormControl>
+								<Input
+									type="file"
+									onChange={(event) => {
+										const file = event.target.files?.[0];
+										if (file) {
+											// Create a URL for the file
+											const url =
+												URL.createObjectURL(file);
+											setPreviewUrl(url); // Update state with the new URL
+										} else {
+											setPreviewUrl(null); // Reset preview URL if no file is selected
+										}
+										field.onChange(file ?? undefined);
+									}}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-					<Button
-						variant="secondary"
-						type="submit"
-						className="w-full"
-						onClick={closePostDialog}
-						disabled={isLoading}>
-						Post
-					</Button>
-				</form>
-			</Form>
-		</div>
+				<Button
+					variant="secondary"
+					type="submit"
+					className="w-full"
+					onClick={closePostDialog}
+					disabled={isLoading}>
+					Post
+				</Button>
+			</form>
+		</Form>
 	);
 };
 
