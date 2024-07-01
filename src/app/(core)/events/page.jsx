a52c7be0d6b2dc05aedc8306f4a9957fc63a23fa
kingoriwangechi <florencekingori00@gmail.com>
2024-07-01 11:55:13 +0300

    feat: Add a card footer to the events card and the RTK Queries and events hooks

--- src/app/(core)/events/page.jsx
+++ src/app/(core)/events/page.jsx
@@ -13,9 +13,10 @@ const Events = () => {
 			{isLoading ? (
 				<p>Loading</p>
 			) : (
-				eventData?.results?.map((event, index) => (
+				eventData?.results?.map((event, post, index) => (
 					<EventCard
 						key={index}
+						eventId={event.id}
 						first_name={event.author.user.first_name}
 						last_name={event.author.user.last_name}
 						isVerified={event.author.is_verified}
@@ -26,12 +27,17 @@ const Events = () => {
 						event_link="https://www.example.com"
 						event_start_date="12th October 2021"
 						event_start_time="12:00 PM"
-						pfpImage={event.profile_picture}
+						pfpImage={event.author.profile_picture}
 						description={event.description}
 						postImage={event.media}
 						size={"large"}
 						type={"Student"}
 						date={formatCreatedAt(event.created_at)}
+						likeCount={event.likes_count}
+						bookmarkCount={event.bookmarks_count}
+						isSaved={event.is_bookmarked}
+						isLiked={event.is_liked}
+						isFollowingCreator={event.is_following_creator}
 					/>
 				))
 			)}
--- src/components/ui/MyComponents/cards/EventCard.jsx
+++ src/components/ui/MyComponents/cards/EventCard.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Card,
 	CardContent,
 	CardFooter,
 	CardHeader,
 } from "@/components/ui/shadcnComponents/card";
-import { CalendarX, MapPin } from "lucide-react";
+import {
+	CalendarX,
+	MapPin,
+	MessageSquareMore,
+	Send,
+	Bookmark,
+	SendHorizonalIcon,
+} from "lucide-react";
+import { Input } from "@/components/ui/shadcnComponents/input";
+import {
+	useLike,
+	useBookmark,
+	useUnlike,
+	useUnbookmark,
+	useComment,
+} from "@/hooks/eventsHooks";
+import Lottie from "react-lottie-player";
+import { LikeSVG, UnLikeSVG } from "@/app/landing/SVGIcon";
 import Link from "next/link";
 import AvatarProfile from "../profile/AvatarProfile";
 import { Button } from "../../shadcnComponents/button";
 import { VerifiedIcon, UserRoundPlus, Globe, Link2 } from "lucide-react";
 import Image from "next/legacy/image";
 import { useProfile } from "@/hooks/profile";
+import { useFollowToggle } from "@/hooks/profile";
+import thumbsUp from "./thumbs-up.json";
+import { useEventsCommentsListQuery } from "@/redux/features/events/eventsApiSlice";
 
 const EventCard = ({
-	postId,
+	eventId,
 	profileId,
 	pfpImage,
 	first_name,
@@ -32,11 +52,55 @@ const EventCard = ({
 	isPhysical,
 	address,
 	isFollowingCreator,
+	isLiked,
+	isSaved,
+	likeCount,
+	bookmarkCount,
 	smallImage,
 }) => {
 	const { data: profileData } = useProfile();
-	console.log("Event Link:", event_link);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [liked, setLiked] = useState(isLiked);
+	const [animationComplete, setAnimationComplete] = useState(false);
 
+	const handleLike = useLike(eventId);
+	const handleUnlike = useUnlike(eventId);
+	const handleBookmark = useBookmark(eventId);
+	const handleUnbookmark = useUnbookmark(eventId);
+
+	const handleFollow = useFollowToggle(1);
+
+	const handleThumbsUp = () => {
+		if (!liked) {
+			handleLike();
+			setIsPlaying(true);
+			setAnimationComplete(false);
+			setTimeout(() => {
+				setIsPlaying(false);
+				setAnimationComplete(true);
+			}, 1000); // Assuming the animation duration is 1 second
+		} else {
+			handleUnlike();
+			setIsPlaying(false);
+			setAnimationComplete(false);
+		}
+		setLiked(!liked);
+	};
+	const [showComment, setShowComment] = useState(false);
+	function toggleComments() {
+		setShowComment(!showComment);
+	}
+	//* comment text
+	const [comment, setComment] = useState("");
+	function handleCommentChange(e) {
+		setComment(e.target.value);
+	}
+	// * comment submission
+	const handleComment = useComment(eventId, comment);
+	function commentSubmit() {
+		handleComment();
+		setComment("");
+	}
 	return (
 		<Card className="flex w-[58%] min-w-[33.25rem] py-[0.3rem] flex-col items-start rounded-[0.5rem] bg-white dark:bg-muted">
 			<CardHeader className="w-full flex flex-row justify-between items-start">
@@ -160,8 +224,120 @@ const EventCard = ({
 					Register
 				</Button>
 			</CardContent>
+			<CardFooter className="flex flex-col w-full gap-1 relative">
+				<div className="flex items-start self-stretch border-y-2">
+					<Button
+						className="flex items-center cursor-pointer"
+						variant="ghost"
+						onClick={handleThumbsUp}>
+						{liked && animationComplete ? (
+							<LikeSVG />
+						) : liked ? (
+							<Lottie
+								animationData={thumbsUp}
+								loop={false}
+								play={isPlaying}
+								className="w-[2rem] h-[2rem] relative bottom-1"
+							/>
+						) : (
+							<UnLikeSVG />
+						)}
+					</Button>
+					<Button
+						variant="ghost"
+						onClick={toggleComments}>
+						<MessageSquareMore />
+					</Button>
+					<Button variant="ghost">
+						<Send />
+					</Button>
+					<Button
+						onClick={!isSaved ? handleBookmark : handleUnbookmark}
+						variant="ghost"
+						className="absolute right-3">
+						{isSaved ? (
+							<Bookmark
+								fill="#00B595"
+								color="#00B595"
+							/>
+						) : (
+							<Bookmark />
+						)}
+					</Button>
+				</div>
+
+				<div className="flex justify-between w-full">
+					<p className="">
+						{" "}
+						{likeCount} {likeCount === 1 ? "Like" : "Likes"}
+					</p>
+					<p className="">
+						{bookmarkCount}{" "}
+						{bookmarkCount === 1 ? "Bookmark" : "Bookmarks"}
+					</p>
+				</div>
+
+				{showComment && (
+					<div className="w-full pt-3 flex flex-col gap-4">
+						<Input
+							type="text"
+							value={comment}
+							onChange={handleCommentChange}
+							placeholder="Write your comment here "
+							suffix={
+								<SendHorizonalIcon
+									className="hover:text-blue-700"
+									onClick={commentSubmit}
+								/>
+							}
+							className="rounded-[6.25rem] border border-[#11294D]"
+						/>
+						<CommentsComponent eventId={eventId} />
+					</div>
+				)}
+			</CardFooter>
 		</Card>
 	);
 };
 
 export default EventCard;
+
+function CommentsComponent({ eventId }) {
+	const { data: commentsList, isLoading } = useEventsCommentsListQuery({
+		event: eventId,
+	});
+	if (commentsList) {
+		console.log(commentsList);
+	}
+	return (
+		<div className="max-h-[20rem] flex flex-col gap-6 overflow-y-scroll">
+			{commentsList?.results?.map((comment, index) => (
+				<div
+					className="flex gap-6 items-start"
+					key={index}>
+					<AvatarProfile
+						pfpImage={comment.author.profile_picture}
+						first_name={comment.author.user.first_name}
+						last_name={comment.author.user.last_name}
+						className="w-[3.2rem] h-[3.2rem]"
+					/>
+					<div className="flex gap-1 flex-col w-full">
+						<p className="muted text-sm flex gap-2">
+							<span>
+								{comment.author.user.first_name}{" "}
+								{comment.author.user.last_name}{" "}
+							</span>
+							<VerifiedIcon
+								fill="#00B595"
+								color="#ffff"
+								className="dark:filter dark:invert"
+								size={20}
+							/>
+						</p>
+						<p className="text-wrap ">{comment.text}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
