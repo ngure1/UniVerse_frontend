3f0bb7db8842a55961ba18a26af8077f3eda98da
ngure1 <ngugijoseph700@gmail.com>
2024-07-05 08:45:49 +0300

    style :gem: modify pages styling

--- src/app/(core)/layout.jsx
+++ src/app/(core)/layout.jsx
@@ -6,10 +6,12 @@ import RequireAuth from "@/redux/features/auth/RequireAuth";
 const layout = ({ children }) => {
 	return (
 		// <RequireAuth>
-		<div className="relative w-full border  bg-gray-200 dark:bg-gray-800 min-h-screen">
-			<NavBar className="fixed top-0 bg-muted z-30" />
-			<Sidebar className="fixed top-[7rem] rounded-sm left-3 bottom-0 bg-white dark:bg-gray-800 z-30 w-[23.5%]" />
-			<div className="mt-[7rem] ml-[27rem]">{children}</div>
+		<div className="flex min-h-screen bg-gray-200 dark:bg-gray-900">
+			<NavBar className="fixed top-0 w-full bg-muted z-30" />
+			<div className="flex-1 ml-[23.5vw] mt-[7rem]">
+				<Sidebar className="w-[23.5vw] fixed top-[7rem] left-2 bottom-2 bg-white dark:bg-gray-900 z-30 rounded-sm" />
+				<div className="pl-[4rem]"> {children}</div>
+			</div>
 		</div>
 		// </RequireAuth>
 	);
