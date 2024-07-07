1dde6e82d129cad211c403f401263b74794636a6
ngure1 <ngugijoseph700@gmail.com>
2024-07-07 16:05:15 +0300

    feat :sparkles: : add tiptap image resizing component

--- src/components/ui/MyComponents/RichTextEditor/TipTap.jsx
+++ src/components/ui/MyComponents/RichTextEditor/TipTap.jsx
@@ -3,13 +3,14 @@
 import { useEffect, useRef } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import ImageResize from "tiptap-extension-resize-image";
 import Toolbar from "./Toolbar";
 import Underline from "@tiptap/extension-underline";
 import Image from "@tiptap/extension-image";
 
 const Tiptap = ({ onChange, content }) => {
 	const editor = useEditor({
-		extensions: [StarterKit, Underline, Image],
+		extensions: [StarterKit, Underline, Image, ImageResize],
 		editorProps: {
 			attributes: {
 				class: "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 items-start w-full gap-3 text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
