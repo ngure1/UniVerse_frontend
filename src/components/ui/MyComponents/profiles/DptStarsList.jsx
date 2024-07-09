81cccc78db81c1e7748e11cb1b1cc2660f0ba017
mwangi-hilda <mwangihw06@gmail.com>
2024-07-09 09:38:11 +0300

    Map profiles data

--- src/components/ui/MyComponents/profiles/DptStarsList.jsx
+++ src/components/ui/MyComponents/profiles/DptStarsList.jsx
@@ -1,10 +1,22 @@
+"use client";
 import React from "react";
 import ProfilesCard from "../cards/ProfilesCard";
+import { useDptStarsListQuery } from "@/redux/features/profileTabs/profilesApiSlice";
 
 const DptStarsList = () => {
+	const { data, isLoading } = useDptStarsListQuery();
+	console.log(data);
 	return (
 		<div className="grid grid-cols-4 bg-white space-y-2 py-4 justify-items-center justify-evenly">
-			<ProfilesCard
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				data?.results?.map((profiles, index) => (
+					<ProfilesCard key={index} />
+				))
+			)}
+
+			{/* <ProfilesCard
 				name="Cate Ndung'u"
 				jobDescription="Software engineer"
 				company="IntelliDJEA"
@@ -28,7 +40,7 @@ const DptStarsList = () => {
 				name="Cate Ndung'u"
 				jobDescription="Software engineer"
 				company="IntelliDJEA"
-			/>
+			/> */}
 		</div>
 	);
 };
