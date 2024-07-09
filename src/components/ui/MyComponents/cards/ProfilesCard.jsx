834439037562d0015ef05789e8a15a18f759313c
mwangi-hilda <mwangihw06@gmail.com>
2024-07-09 11:43:49 +0300

    Add props to avatar profile, add link to avatar profile to redirect one to the user's profile

--- src/components/ui/MyComponents/cards/ProfilesCard.jsx
+++ src/components/ui/MyComponents/cards/ProfilesCard.jsx
@@ -3,18 +3,29 @@ import { Card } from "../../shadcnComponents/card";
 import AvatarProfile from "../profile/AvatarProfile";
 import { Button } from "../../shadcnComponents/button";
 import { UserRoundPlus } from "lucide-react";
+import Link from "next/link";
 
-const ProfilesCard = ({ name, jobDescription, company, pfpImage }) => {
+const ProfilesCard = ({
+	firstName,
+	lastName,
+	jobDescription,
+	company,
+	pfpImage,
+	profilesId,
+}) => {
 	return (
 		<Card className="relative flex items-center shadow-lg flex-col p-2 w-[15rem] h-[20rem]">
-			<div className="">
-				<div className="w-full h-[5rem] bg-gray-400 absolute top-0 left-0 z-10"></div>
+			<div className="w-full h-[5rem] bg-gray-400 absolute top-0 left-0 z-10"></div>
+			<Link href={`/userprofile/${profilesId}`}>
 				<AvatarProfile
 					className="w-[7rem] h-[7rem] mb-2 relative z-30"
-					// pfpImage={postImage}
+					pfpImage={pfpImage}
 				/>
-			</div>
-			<p className="body-md">{name}</p>
+				<p className="body-md no-underline hover:underline">
+					{firstName} {lastName}
+				</p>
+			</Link>
+
 			<p className="text-sm">{jobDescription}</p>
 			<p className="text-sm">{company}</p>
 			<Button
