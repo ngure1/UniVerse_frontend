693b5362376febf5d8c86b69321a9132b69925ae
mwangi-hilda <mwangihw06@gmail.com>
2024-07-10 13:03:18 +0300

    Add features to job page

--- src/app/(core)/jobs/page.jsx
+++ src/app/(core)/jobs/page.jsx
@@ -1,12 +1,62 @@
 "use client";
 import React from "react";
-import JobCard from "@/components/ui/MyComponents/cards/JobCard";
+import JobDetailCard from "@/components/ui/MyComponents/cards/JobDetailCard";
 import Logo from "@/../public/images/logo.png";
+import JobForm from "@/components/forms/jobForm";
+import AvatarProfile from "@/components/ui/MyComponents/profile/AvatarProfile";
+import {
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/shadcnComponents/dialog";
+import { BriefcaseBusiness } from "lucide-react";
+import { Button } from "@/components/ui/shadcnComponents/button";
+import { Card } from "@/components/ui/shadcnComponents/card";
+import JobsCard from "@/components/ui/MyComponents/cards/JobsCard";
 
 const Jobs = () => {
 	return (
 		<div>
-			<JobCard
+			<Card className="flex w-[58%] mb-[2rem] min-w-[33.25rem] py-[0.6rem] flex-col items-start rounded-[0.5rem] bg-white dark:bg-muted">
+				<div className="flex w-full m-2">
+					<AvatarProfile
+						className="w-[3rem] h-[3rem]"
+						// pfpImage={profileData?.profile_picture}
+						// first_name={profileData?.user.first_name}
+						// last_name={profileData?.user.last_name}
+					/>
+					<Dialog className="">
+						<DialogTrigger asChild>
+							<Button
+								variant="outline"
+								className="gap-1 w-full h-[5vh] rounded-[20px] mx-[1rem] ">
+								<BriefcaseBusiness color="#90B494" />
+								Post a New Job
+							</Button>
+						</DialogTrigger>
+						<DialogContent className="sm-max w-md">
+							<DialogTitle>Jobs</DialogTitle>
+							<JobForm />
+						</DialogContent>
+					</Dialog>
+				</div>
+			</Card>
+
+			<JobsCard
+				job_title="Executive Partner"
+				company="Athena"
+				address="Kiambu, Kenya"
+				job_type="On-site"
+			/>
+			<JobsCard
+				job_title="Executive Partner"
+				company="Athena"
+				address="Kiambu, Kenya"
+				job_type="On-site"
+			/>
+
+			{/* <JobDetailCard
 				companyLogo={Logo}
 				companyName="UniVerse"
 				jobTitle="Data Analyst"
@@ -39,7 +89,7 @@ const Jobs = () => {
 				Please rest assured that our application process is revised by humans who are really interested in learning about you, and your application form is a great way for us to do that. Therefore, we do not accept email applications.
 				Please note that only shortlisted candidates will be contacted for follow-up.
 				If you have any technical issues applying, please contact us at info@decodis.com."
-			/>
+			/> */}
 		</div>
 	);
 };
