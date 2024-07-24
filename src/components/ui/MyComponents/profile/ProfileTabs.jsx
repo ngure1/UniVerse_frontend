956f01bfb8df15bd434b79e98e5dd1f2f0b1809f
kingoriwangechi <florencekingori00@gmail.com>
2024-07-24 13:40:56 +0300

    chore: Refactor ProfileTabs component to use shadcnComponents and lazy load education data

--- src/components/ui/MyComponents/profile/ProfileTabs.jsx
+++ src/components/ui/MyComponents/profile/ProfileTabs.jsx
@@ -18,8 +18,21 @@ import {
 	Grid3X3,
 	CalendarRange,
 } from "lucide-react";
+import { useEducationListQuery } from "@/redux/features/educationList/educationApiSlice";
+import {
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/shadcnComponents/dialog";
+import EducationListForm from "@/components/forms/educationListForm";
+import { Button } from "@/components/ui/shadcnComponents/button";
+import { Plus } from "lucide-react";
 
 const ProfileTabs = ({ user_id, is_owner }) => {
+	const { data: educationData, isLoading } = useEducationListQuery(1);
+	console.log(educationData);
+
 	return (
 		<Tabs
 			defaultValue="posts"
@@ -73,7 +86,38 @@ const ProfileTabs = ({ user_id, is_owner }) => {
 					<WorkList />
 				</TabsContent>
 				<TabsContent value="education">
-					<EducationList />
+					<div className="flex flex-col ">
+						<Dialog className="">
+							<DialogTrigger asChild>
+								<Button
+									variant="outline"
+									className="gap-1 w-[50%] h-[5vh] mb-2 ">
+									<Plus color="#90B494" />
+									Add Education Details
+								</Button>
+							</DialogTrigger>
+							<DialogContent className="sm-max w-md">
+								<DialogTitle>Education</DialogTitle>
+								<EducationListForm />
+							</DialogContent>
+						</Dialog>
+						{isLoading ? (
+							<p>Loading...</p>
+						) : (
+							educationData?.results?.map((education, index) => (
+								<div>
+									<EducationList
+										institution_name={
+											education.institution_name
+										}
+										fieldOfStudy={education.field_of_study}
+										startDate={education.start_date}
+										endDate={education.end_date}
+									/>
+								</div>
+							))
+						)}
+					</div>
 				</TabsContent>
 				<TabsContent value="savedPosts">
 					<SavedPosts
