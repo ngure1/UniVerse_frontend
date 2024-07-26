1449832bde8483371a135156ac85966b7cad343b
kingoriwangechi <florencekingori00@gmail.com>
2024-07-26 09:55:52 +0300

    refactor: Update educationListSchema to use "institution_name" instead of "institution"

--- src/schema/educationListSchema.js
+++ src/schema/educationListSchema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const educationListSchema = z.object({
-	institution: z.string().nonempty("Instituition is required"),
+	institution_name: z.string().nonempty("Instituition is required"),
 	field_of_study: z.string().nonempty("Field is required"),
 	start_date: z.string().nonempty("Date is required"),
 	end_date: z.string().nonempty("Date is required"),
