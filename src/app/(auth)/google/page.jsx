3e0c01b04b147bc89dee2751f8f5ac4c8ec2db2a
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-30 23:21:16 +0300

    fix sign in with google bugs ... (post request didnt work on first trial, login page was momentarily displayed)

--- src/app/(auth)/google/page.jsx
+++ src/app/(auth)/google/page.jsx
@@ -2,11 +2,19 @@
 import useGoogleAuth from "@/hooks/continue-with-google-hook";
 
 function Page() {
-	useGoogleAuth();
+	const loading = useGoogleAuth();
+
+	if (loading) {
+		return (
+			<div>
+				<h1>Signing in with Google...</h1>
+			</div>
+		);
+	}
 
 	return (
 		<div>
-			<h1>Signing in with google...</h1>
+			<h1>Redirecting...</h1>
 		</div>
 	);
 }
